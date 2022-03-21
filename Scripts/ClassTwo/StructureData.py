e93783a350719f2069b1d693644c9c84c252581e
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 20:51:19 -0300

    Percorrendo uma lista e imprimindo sua posiçao e valor.

--- Scripts/ClassTwo/StructureData.py
+++ Scripts/ClassTwo/StructureData.py
@@ -5,14 +5,25 @@
 # print(f"Quantidade de y no texto: {text.count('a')}")
 # print(f"As 5 primeiras letra são: {text[0:10]}")
 
+#-------------
 # Uso do meétodo split()
 
-text = "Aprendendo, Python, na disciplina de linguagem de progrmação"
+# text = "Aprendendo, Python, na disciplina de linguagem de progrmação"
 
-print(f"texto: {text}")
-print(f"Tamanho do texto: {len(text)}\n")
+# print(f"texto: {text}")
+# print(f"Tamanho do texto: {len(text)}\n")
 
-worlds = text.split(',')
-print(f"Palavras: {worlds}")
-print(f"Tamanho das palavras: {len(worlds)}")
+# worlds = text.split(',')
+# print(f"Palavras: {worlds}")
+# print(f"Tamanho das palavras: {len(worlds)}")
+
+#-------------
+# Listas
+
+vogais = ['a', 'e', 'i', 'o', 'u']
+
+for vogal in vogais:
+    print(f"Posição: {vogais.index(vogal)}, valor: {vogal}")
+
+
 
