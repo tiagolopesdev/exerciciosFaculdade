a5a77f9ab783ac3508242b191e2d599103cffc28
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 20:31:51 -0300

    Pegando a quantidade de elementos contidos em uma variavel do tipo String com metodo count().

--- Scripts/ClassTwo/StructureData.py
+++ Scripts/ClassTwo/StructureData.py
@@ -2,3 +2,4 @@ text = "Aprendendo Python na disciplina de linguagem de progrmação"
 
 print(f"Tamanho do texto: {len(text)}")
 print(f"Python no texto: {'Python' in text}")
+print(f"Quantidade de y no texto: {text.count('a')}")
