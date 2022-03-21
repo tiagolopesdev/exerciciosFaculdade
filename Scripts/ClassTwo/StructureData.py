7b0bb8ed510443401018bd248210842637ad3557
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 20:34:11 -0300

    /bin/python3 /home/tiagolopes/Aula_faculdade/Scripts/ClassTwo/StructureData.py

--- Scripts/ClassTwo/StructureData.py
+++ Scripts/ClassTwo/StructureData.py
@@ -3,3 +3,4 @@ text = "Aprendendo Python na disciplina de linguagem de progrmação"
 print(f"Tamanho do texto: {len(text)}")
 print(f"Python no texto: {'Python' in text}")
 print(f"Quantidade de y no texto: {text.count('a')}")
+print(f"As 5 primeiras letra são: {text[0:6]}")
