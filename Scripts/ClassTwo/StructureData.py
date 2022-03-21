46e68b05b4f1eb5fe0433dca41e1b0a41368945b
tiagoLopes <saxtiago14@gmail.com>
2022-03-21 20:45:16 -0300

    Uso do metodo split().

--- Scripts/ClassTwo/StructureData.py
+++ Scripts/ClassTwo/StructureData.py
@@ -1,6 +1,18 @@
-text = "Aprendendo Python na disciplina de linguagem de progrmação"
+# text = "Aprendendo Python na disciplina de linguagem de progrmação"
 
-print(f"Tamanho do texto: {len(text)}")
-print(f"Python no texto: {'Python' in text}")
-print(f"Quantidade de y no texto: {text.count('a')}")
-print(f"As 5 primeiras letra são: {text[0:10]}")
+# print(f"Tamanho do texto: {len(text)}")
+# print(f"Python no texto: {'Python' in text}")
+# print(f"Quantidade de y no texto: {text.count('a')}")
+# print(f"As 5 primeiras letra são: {text[0:10]}")
+
+# Uso do meétodo split()
+
+text = "Aprendendo, Python, na disciplina de linguagem de progrmação"
+
+print(f"texto: {text}")
+print(f"Tamanho do texto: {len(text)}\n")
+
+worlds = text.split(',')
+print(f"Palavras: {worlds}")
+print(f"Tamanho das palavras: {len(worlds)}")
+
