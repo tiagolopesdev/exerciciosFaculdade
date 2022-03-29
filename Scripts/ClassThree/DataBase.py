bdbf1be2dd78dab1102b4471b86f12529eb3fa13
tiagoLopes <saxtiago14@gmail.com>
2022-03-28 23:31:56 -0300

    Arquivo de BD

--- Scripts/ClassThree/DataBase.py
+++ Scripts/ClassThree/DataBase.py
@@ -2,3 +2,25 @@ import sqlite3 as sql
 
 conn = sql.connect('aulaDB.db')
 print(type(conn))
+
+ddlCreate = """CREATE TABLE fornecedor (
+    id_fornecedor INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    conn = sqlite3.connect('aulaDB.db')
+    nome_fornecedor TEXT NOT NULL,
+    print(type(conn))
+    cnpj VARCHAR(18) NOT NULL,
+    cidade TEXT,
+    estado VARCHAR(2) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    data_cadastro DATE NOT NULL
+);"""
+
+cursor = conn.cursor()
+cursor.execute(ddlCreate)
+print(type(cursor))
+
+print("Tabela criada!")
+print("Descricao do cursor: "+cursor.description)
+print("Linhas afetadas:"+cursor.rowcount)
+cursor.close()
+conn.close()
