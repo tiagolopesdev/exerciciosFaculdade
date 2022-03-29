985a25e075ba48b56fe9c4dd295b57d6e1b273d2
tiagoLopes <saxtiago14@gmail.com>
2022-03-28 21:04:46 -0300

    Importando funcionalidades da biblioteca.

--- Scripts/ClassThree/bibliotecasPython.py
+++ Scripts/ClassThree/bibliotecasPython.py
@@ -1,7 +1,11 @@
-import math as m
+# import math as m
 
-print(m.sqrt(25))
-print(m.log2(1024))
-print(m.cos(45))
+# print(m.sqrt(25))
+# print(m.log2(1024))
+# print(m.cos(45))
 
+from math import sqrt, log2, cos
 
+print(sqrt(25))
+print(log2(1024))
+print(cos(45))
