bfc61dea5158c2e57643a7197f861af40cb24f11
tiagoLopes <saxtiago14@gmail.com>
2022-04-03 17:04:06 -0300

    Adicionando dados através de um dicionário acessível a indices.

---
+++ Scripts/ClassFour/LibraryPandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+pd.Series(data=5)
+
+dados = {
+    'nome1': 'Howard',
+    'nome2': 'Ian',
+    'nome3': 'Peter',
+    'nome4': 'Jonah',
+    'nome5': 'Kellie',
+}
+print(pd.Series(dados))
