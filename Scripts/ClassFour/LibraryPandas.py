4dd4474be78bc905aea4a538a4cf7651f2421f0d
tiagoLopes <saxtiago14@gmail.com>
2022-04-03 17:19:11 -0300

    Adicionando como o indice dos nomes o CPF.

--- Scripts/ClassFour/LibraryPandas.py
+++ Scripts/ClassFour/LibraryPandas.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 pd.Series(data=5)
 
+listNames = 'Tiago João Lucas Bia Ana'.split()
+
+print(pd.Series(listNames))
+
+# Cria uma serie com um dicionário
 dados = {
     'nome1': 'Howard',
     'nome2': 'Ian',
@@ -9,4 +14,15 @@ dados = {
     'nome4': 'Jonah',
     'nome5': 'Kellie',
 }
-print(pd.Series(dados))
+pd.Series(dados) 
+
+cpfs = '''111.111.111-11 
+    222.222.222-22 333.333.333-33 
+    444.444.444-44 555.555.555-55
+    '''.split()
+
+print("\n")
+
+pd.Series(listNames, index=cpfs)
+
+
